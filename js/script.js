354bf510201d7254e8da563587249e09eb67a0a1
Chuma Nnaji <cnnaji2003@gmail.com>
2012-09-30 17:09:32 -0700

    Fixed hex issues and expanded margin.

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ $(function() {
 	$('.color').each(function(index, element) {
 		if ($(element).prop('id') == 'title-color') {
 			$(element).on('input', function(e) {
-				$('.' + $(this).prop('id')).css('color', '#' + $(this).val());
+				$('.' + $(this).prop('id')).css('color', $(this).val());
 			});
 		} else {
 			$(element).on('input', function(e) {
-				$('.' + $(this).prop('id')).css('background-color', '#' + $(this).val());
+				$('.' + $(this).prop('id')).css('background-color', $(this).val());
 			});
 		}
 		$(element).trigger('input');
