bfd6cdac0ac4453b14a4825a5ac003779db000c4
Chuma Nnaji <cnnaji2003@gmail.com>
2017-02-14 00:56:22 -0800

    Clone themes

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ from google.appengine.ext import db
 jinja_environment = jinja2.Environment(
             loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+class Theme(db.Model):
+    author = db.StringProperty()
+    title = db.StringProperty()
+    title_color = db.StringProperty()
+    background_color = db.StringProperty()
+    main_color = db.StringProperty()
+    options_color = db.StringProperty()
+
 
 class ColorTheme(db.Model):
     title = db.StringProperty()
@@ -30,15 +38,20 @@ class MainHandler(webapp2.RequestHandler):
 class ThemeJsonHandler(webapp2.RequestHandler):
     def get(self):
         themes = db.GqlQuery("SELECT author, title, title_color, background_color, main_color, options_color "
-           "FROM ColorTheme WHERE approved = 1 "
-           "ORDER BY date DESC")
+           "FROM Theme")
+
+        for t in themes:
+            c = ColorTheme()
+            c.author = t.author
+            c.title = t.title
+            c.title_color = t.title_color
+            c.background_color = t.background_color
+            c.main_color = t.main_color
+            c.options_color = t.options_color
+            c.put()
 
         self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
-        outThemes = []
-        for t in themes:
-            outThemes.append(db.to_dict(t))
-
-        self.response.write(json.dumps(outThemes))
+        self.response.out.write(200)
 
 
 class NewThemeHandler(webapp2.RequestHandler):
